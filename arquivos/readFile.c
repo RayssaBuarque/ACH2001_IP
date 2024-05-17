c95334261941bb1c34de06fbb231b3e8270616a3
Rayssa Buarque <102766434+RayssaBuarque@users.noreply.github.com>
2024-05-17 18:37:44 -0300

    Notas e contagem de tempo readFile.c

--- arquivos/readFile.c
+++ arquivos/readFile.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <time.h>
 
 #define BUFLEN 100
 
 void main () {
+  struct timespec t_inicio;
+  struct timespec t_final;
+  int tempo_execucao_sec;
+  int tempo_execucao_nsec;
+
+  // pegando o timestamp do Sistema operacional no momento em q a linha roda e alocando ele na var t_inicio	
+  clock_gettime(CLOCK_MONOTONIC, &t_inicio);
+
+  // alguma coisa de leitura de arquivos, pesquisa dps
   char buf[BUFLEN];
   FILE *fp;
   fp=fopen ("texto.txt", "rt");
@@ -17,4 +27,12 @@ void main () {
     }
   }
   fclose(fp);
+  // pegando o timestamp do Sistema operacional no momento em q a linha roda e alocando ele na var t_final
+  clock_gettime(CLOCK_MONOTONIC, &t_final);
+
+  // calculando a diferença de tempo em segundos e nanosegundos do timestamp inicial ao final, e printando na tela
+  tempo_execucao_sec = (t_final.tv_sec - t_inicio.tv_sec);
+  tempo_execucao_nsec = (t_final.tv_nsec - t_inicio.tv_nsec);
+  printf("TEMPO DE EXECUÇÃO: %dsec %dnsec\n", tempo_execucao_sec, tempo_execucao_nsec); 
+
 }
