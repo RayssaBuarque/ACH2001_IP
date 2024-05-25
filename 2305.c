4f6dfb6271bc27ad89dbbd9ffd92bcafb5e1a22b
rayssabuarque <brayssa17@gmail.com>
2024-05-25 19:48:01 -0300

    ajeitando comentários e funções

--- 2305.c
+++ 2305.c
@@ -4,8 +4,8 @@
 #include <time.h>
 
 // Função para gerar os números aleatórios
-// Vai retornar um ponteiro de inteiro e recebe o tamanho do array para gerar os
-// números em loop
+// Vai retornar um ponteiro de inteiro e recebe o tamanho do array para gerar os números em loop
+
 int *sortearNumeros(int tamanhoArray) {
   // Ponteiro que aponta para a memória alocada de maneira dinâmica para o array
   // de números aleatórios
@@ -19,7 +19,7 @@ int *sortearNumeros(int tamanhoArray) {
     manual por meio do comando "free" - caso contrário, ocorrem erros de memory
     leak. Internamente, malloc mantém uma lista de blocos de memória, rodando o
     comando, ele atribui um apto para suportar o tamanho requisitado em bytes
-      isso tudo depende de quais estiverem livres, por isso a importância do
+    isso tudo depende de quais estiverem livres, por isso a importância do
     comando "free". "são globais até que se prove o contrário"
   */
 
@@ -33,23 +33,19 @@ int *sortearNumeros(int tamanhoArray) {
   return numsSorteados;
 }
 
-// callback do qsort:
-int compare(const void *p,
-            const void *q) // qsort envia ponteiros de dois valores da vítima
+// callback do Qsort:
+int compare(const void *p, const void *q) // qsort envia ponteiros de dois valores da vítima
 {
   // Busca o valor numérico dos ponteiros
   int v1 = *(const int *)p;
   int v2 = *(const int *)q;
 
-  if (v1 < v2) { // se v1>v2, seu retorno é <0, isso quer dizer que v1 vem ANTES
+  if (v1 < v2) { // se (v1>v2), seu retorno é (<0), isso quer dizer que v1 vem ANTES
     return -1;
   }
-
-  else if (v1 > v2) { // se v1<v2, seu retorno é >0, isso quer dizer que v1 vem
-                      // DEPOIS
+  else if (v1 > v2) { // se (v1<v2), seu retorno é (>0), isso quer dizer que v1 vem DEPOIS
     return 1;
   }
-
   else { // se v1=v2, ele retorna 0.
     return 0;
   }
@@ -64,11 +60,12 @@ void printuf(int *numeros, int arraySize) {
 
 // odernação dos números através do insertionsort, a funcao recebe o array
 // gerado aleatoriamente (desordenado) e seu tamanho
-int *insertionSort(int *numsDesordenados, int tArray) {
+int *insertionSort(int *numsDesordenados, int tamanhoArray) {
 
-  for (int j = 1; j < tArray; j++) {
+  for (int j = 1; j < tamanhoArray; j++) {
     int key = numsDesordenados[j];
     int i = j - 1;
+    
     while ((i > 0) && (numsDesordenados[i] > key)) {
       numsDesordenados[i + 1] = numsDesordenados[i];
       i = i - 1;
@@ -76,11 +73,11 @@ int *insertionSort(int *numsDesordenados, int tArray) {
     numsDesordenados[i + 1] = key;
   }
 
-  // embora a variavel chame-se "numsDesordenados", eles estão ordernados após
-  // passarem pelo loop
+  // "numsDesordenados" torna-se um array ordenado após o loop
   return numsDesordenados;
 }
 
+// função que ordena as arrays
 int *ordenararrays(int tipoOrdenacao, int tamanhoArray) {
 
   // declarações para medir o tempo
@@ -89,29 +86,21 @@ int *ordenararrays(int tipoOrdenacao, int tamanhoArray) {
   struct timespec tInicioIsort;
   struct timespec tFinalIsort;
 
-  // arrays que guardarao o resultado do tempo de execucao, [0] armazena o tempo
-  // em ns e [1] o tempo em s
-  int *tempo_execucaoQ = (int *)malloc(tamanhoArray * sizeof(int));
+  // arrays que guardarao o resultado do tempo de execucao,
+  // [0] = tempo em ns
+  // [1] = tempo em s
   int *tempo_execucaoI = (int *)malloc(tamanhoArray * sizeof(int));
+  int *tempo_execucaoQ = (int *)malloc(tamanhoArray * sizeof(int));
 
   // quando ordenaarrays é chamada com o parametro '1' ela gera o array
   // aleatoriamente e o organiza com 'qsort'
   if (tipoOrdenacao == 1) {
     int *sorteioA = sortearNumeros(tamanhoArray);
-
-    // printf("Array desordenado:");
-    // printuf(sorteioA, tamanhoArray);
-
-    // tempo de sistema antes do qsort rodar
+    
+    // capturando o tempo de sistema antes e depois do qsort rodar
     clock_gettime(CLOCK_MONOTONIC, &tInicioQsort);
-
-    qsort(sorteioA, tamanhoArray, sizeof(int), compare);
-
-    // tempo de sistema após o qsort rodar
+        qsort(sorteioA, tamanhoArray, sizeof(int), compare);
     clock_gettime(CLOCK_MONOTONIC, &tFinalQsort);
-
-    // printf("\nArray ordenado:");
-    // printuf(sorteioA, tamanhoArray);
 
     // tempo total = tempo final - tempo inicial
     tempo_execucaoQ[0] = tFinalQsort.tv_nsec - tInicioQsort.tv_nsec;
@@ -127,16 +116,10 @@ int *ordenararrays(int tipoOrdenacao, int tamanhoArray) {
   else if (tipoOrdenacao == 2) {
     int *sorteioB = sortearNumeros(tamanhoArray);
 
-    // pra ver se funciona
-    // printf("\n Array desordenado:");
-    // printuf(sorteioB, tamanhoArray);
-
+    // capturando o tempo de sistema antes e depois do qsort rodar
     clock_gettime(CLOCK_MONOTONIC, &tInicioIsort);
-    int *ordenaB = insertionSort(sorteioB, tamanhoArray);
+        int *ordenaB = insertionSort(sorteioB, tamanhoArray);
     clock_gettime(CLOCK_MONOTONIC, &tFinalIsort);
-
-    // printf("\n Array ordenado:");
-    // printuf(ordenaB, tamanhoArray);
 
     tempo_execucaoI[0] = tFinalIsort.tv_nsec - tInicioIsort.tv_nsec;
     tempo_execucaoI[1] = tFinalIsort.tv_sec - tInicioIsort.tv_sec;
@@ -153,21 +136,23 @@ int main() {
   scanf("%d", &arraySize);
 
   // chama a funçao de ordenacao no modo desejado e recebe o tempo de execucao
-  int *qsort = ordenararrays(1, arraySize);
+  int *dadoQsort = ordenararrays(1, arraySize);
   int *insertionsort = ordenararrays(2, arraySize);
 
   printf("\n Tempo execução QSORT:");
-  printf("\n %dns %ds", qsort[0], qsort[1]);
+  printf("\n %dns %ds", dadoQsort[0], dadoQsort[1]);
 
   printf("\n\n Tempo execução INSERTIONSORT:");
   printf("\n %dns %ds", insertionsort[0], insertionsort[1]);
 
-  if (qsort[0] > insertionsort[0]) {
+  if (dadoQsort[0] > insertionsort[0]) {
     printf("\n\n QSORT demorou mais");
-  } else if (insertionsort[0] > qsort[0]) {
+  } else if (insertionsort[0] > dadoQsort[0]) {
     printf("\n\n INSERTION-SORT demorou mais");
   }
-  free(qsort);
+  
+  // liberando espaços de memória
+  free(dadoQsort);
   free(insertionsort);
 
   return 0;
